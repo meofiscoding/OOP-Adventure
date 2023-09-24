1380cc80d866e0ff1061c1170c8029090362bb37
meof-coding <tranthe150186@fpt.edu.vn>
2023-09-25 01:18:21 +0700

    fix: set width and height of house

--- oop-advanture/Src/Map/House.Room.cs
+++ oop-advanture/Src/Map/House.Room.cs
@@ -14,6 +14,8 @@ namespace oop_advanture.Src.Map
         // Using node structure to create room
         public void CreateRoom(int width, int height)
         {
+            Width = Math.Clamp(width, 2, 10);
+            Height = Math.Clamp(height, 2, 10);
             var total = width * height;
             // Create room
             Rooms = new Room[total];
@@ -26,17 +28,21 @@ namespace oop_advanture.Src.Map
                 tmpRoom.Name = String.Format(Text.Language.DefaultRoomName, i, col, row);
 
                 // Calculate neighbor room index
-                if(col < Width - 1){
+                if (col < Width - 1)
+                {
                     tmpRoom.Neighbors[Direction.East] = CalculateRoomIndex(col + 1, row);
                 }
                 // col > 0 && col > Width - 1
-                if(col > 0){
+                if (col > 0)
+                {
                     tmpRoom.Neighbors[Direction.West] = CalculateRoomIndex(col - 1, row);
                 }
-                if(row < Height - 1){
+                if (row < Height - 1)
+                {
                     tmpRoom.Neighbors[Direction.South] = CalculateRoomIndex(col, row + 1);
                 }
-                if(row > 0){
+                if (row > 0)
+                {
                     tmpRoom.Neighbors[Direction.North] = CalculateRoomIndex(col, row - 1);
                 }
                 Rooms[i] = tmpRoom;
