0568fbbf2b0aa1203df34c7fdd9362c72f92c7c3
meof-coding <tranthe150186@fpt.edu.vn>
2023-09-26 12:34:37 +0700

    feat: add inventory to store item

---
+++ oop-advanture/Items/Inventory.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace oop_advanture.Items
+{
+    public class Inventory
+    {
+        private List<Item> Items { get; } = new();
+        public int Total => Items.Count;
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void Remove(Item item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
